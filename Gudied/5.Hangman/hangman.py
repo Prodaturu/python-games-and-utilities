e9678033004001129665aa2c54948ab293930a0c
Jaggyjazz <95521128+Jaggyjazz@users.noreply.github.com>
2023-03-28 10:29:41 +0200

    Hangman

---
+++ Gudied/5.Hangman/hangman.py
@@ -0,0 +1,59 @@
+import random
+import string
+from hang_draw import lives_visual_dict
+from words import words
+
+def get_valid_word(words):
+    word = random.choice(words)  # randomly chooses something from the list
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word.lower()
+
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)  # letters in the word
+    alphabet = set(string.ascii_lowercase)
+    used_letters = set()  # what the user has guessed
+
+    lives_left = 7
+
+    # getting user input
+    while len(word_letters) > 0 and lives_left > 0:
+        # letters used
+        # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
+        print('You have', lives_left, 'lives left and you have used these letters: ', ' '.join(used_letters))
+
+        # what current word is (ie W - R D)
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print(lives_visual_dict[lives_left])
+        print('Current word: ', ' '.join(word_list))
+
+        user_letter = input('Guess a letter: ').lower()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+                print('')
+
+            else:
+                lives_left = lives_left - 1  # takes away a life if wrong
+                print('\n', 'Your letter,', user_letter, 'not in the word.')
+
+        elif user_letter in used_letters:
+            print('\n', 'Used letter. Guess again.')
+
+        else:
+            print('\n','Not a valid letter.')
+
+    #ä here when len(word_letters) == 0 OR when lives == 0
+    if lives_left == 0:
+        print(lives_visual_dict[lives])
+        print('The man is Hanged!! The word is:', word)
+    else:
+        print('YAY! You guessed the word', word, '!!')
+
+
+if __name__ == '__main__':
+    hangman()
---
+++ Gudied/5.Hangman/hangman1.py
@@ -0,0 +1,20 @@
+import random
+from words import words 
+from hang_draw import lives_visual_dict
+
+customWords=input('Want to add custom words into pool of words: y/n')
+if (customWords == 'y'):
+  customWords=input('Enter your custom words with "," separating them: ').split(',')
+  words.extend(customWords)
+elif (customWords != 'y' and customWords!= 'n' ):
+  print('Invalid input. Default words will be used')
+
+#+ 1) computer picks a valid word from available list of words
+def validWords(words):
+  secret_word = random.choice(words) # choose random word in words list
+  while '_' or ' ' in secret_word:
+    secret_word = random.choice(words)
+  return secret_word.lower()
+
+def hangman():
+  
