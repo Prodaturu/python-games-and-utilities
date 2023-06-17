7b181d1c558c27a3dd9278b815c1b105b22e05be
Jaggyjazz <95521128+Jaggyjazz@users.noreply.github.com>
2023-06-17 13:45:56 +0530

    Deleted Excess directories
    Deleted random directory where random and unrelated projects were added to this repo.

--- Random/tiny/map_function.py
+++
@@ -1,8 +0,0 @@
-def double_number(number):
-    return number*2
-
-print(list(map(double_number,[1,2,3])))
-
-print(list(map(lambda num: num*2,[1,2,3])))
-print(list(map(lambda num: num**2,[1,2,3])))
-
